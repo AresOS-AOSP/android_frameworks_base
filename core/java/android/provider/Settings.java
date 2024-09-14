0ac9a288e75fc6c3439c20e60dd97416888390f2
someone5678 <someone5678@users.noreply.github.com>
2024-09-14 15:24:13 +0530

    base: Move window ignore secure to global settings
    Change-Id: Iea0353311216b3721cebccf731c50395bca89e2e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14783,13 +14783,6 @@ public final class Settings {
                 "otp_redaction_lock_time";
 
         /**
-         * Control whether FLAG_SECURE is ignored for all windows.
-         * @hide
-         */
-        @Readable
-        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
-
-        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
@@ -21446,6 +21439,13 @@ public final class Settings {
          * @hide
          */
         public static final String NO_STORAGE_RESTRICT = "no_storage_restrict";
+
+        /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
          * Setting indicating whether Low Power Standby is enabled, if supported.
