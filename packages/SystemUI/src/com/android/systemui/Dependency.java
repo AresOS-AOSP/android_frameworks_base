d8b3b17c1f149eb0f1dad6fa2eecb7913b7d3a71
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-03 18:04:09 +0800

    [axion_ui] adding common slider tiles impl
    common impl for qs tiles
    
    [ChrisCatto]: Removed mentions of torch and flashlight control.
    
    Change-Id: I6e6eb5e3b6230334df454467f8d5f6bd3ed45ca4
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -40,6 +40,7 @@ import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.PluginManager;
 import com.android.systemui.plugins.VolumeDialogController;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.tiles.impl.AxTileProvider;
 import com.android.systemui.recents.LauncherProxyService;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.CommandQueue;
@@ -164,6 +165,7 @@ public class Dependency {
     @Inject Lazy<UserTracker> mUserTrackerLazy;
     @Inject Lazy<StatusBarWindowControllerStore> mStatusBarWindowControllerStoreLazy;
     @Inject @Background Lazy<Executor> mBackgroundExecutor;
+    @Inject Lazy<AxTileProvider> mAxTileProvider;
 
     @Inject
     public Dependency() {
@@ -211,7 +213,8 @@ public class Dependency {
         mProviders.put(
                 StatusBarWindowControllerStore.class, mStatusBarWindowControllerStoreLazy::get);
         mProviders.put(BACKGROUND_EXECUTOR, mBackgroundExecutor::get);
-
+        mProviders.put(
+                AxTileProvider.class, mAxTileProvider::get);
         Dependency.setInstance(this);
     }
 
