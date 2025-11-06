db617853445ef4c909edc8da10b4286746df393d
priiii08918 <priyanshusinghal0818@proton.me>
2025-11-06 09:37:03 +0000

    Revert "SystemUI: Add tile to show volume panel"
    This reverts commit 09efdc754b3bff2b36ea447aa41410bc8e5e4af4.
    
    Change-Id: I53fd13d5c5d1e7172a8b04d8f518810dc8d93439

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -45,7 +45,6 @@ import com.android.systemui.qs.tiles.SmartPixelsTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
-import com.android.systemui.qs.tiles.VolumeTile
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WeatherTile
 import com.android.systemui.qs.tiles.WifiTile
@@ -193,12 +192,6 @@ interface LineageModule {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
-
-    /** Inject VolumeTile into tileMap in QSModule */
-    @Binds
-    @IntoMap
-    @StringKey(VolumeTile.TILE_SPEC)
-    fun bindVolumeTile(volumeTile: VolumeTile): QSTileImpl<*>
 
     /** Inject VpnTile into tileMap in QSModule */
     @Binds
@@ -561,21 +554,6 @@ interface LineageModule {
                 ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.ACCESSIBILITY
-            )
-        }
-
-        @Provides
-        @IntoMap
-        @StringKey(VolumeTile.TILE_SPEC)
-        fun provideVolumeTile(uiEventLogger: QsEventLogger): QSTileConfig {
-            return QSTileConfig(
-                tileSpec = TileSpec.create(VolumeTile.TILE_SPEC),
-                uiConfig = QSTileUIConfig.Resource(
-                    iconRes = R.drawable.ic_qs_volume_panel,
-                    labelRes = R.string.quick_settings_volume_panel_label
-                ),
-                instanceId = uiEventLogger.getNewInstanceId(),
-                category = TileCategory.UTILITIES
             )
         }
 
