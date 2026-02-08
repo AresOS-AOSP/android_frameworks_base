a65014ab40493cb1572a891bf1b9497710332757
Ghosuto <clash.raja10@gmail.com>
2026-02-08 12:50:32 +0000

    SystemUI: Allow disable statusbar combined signal
    - Thanks crdroid for icon
    
    Change-Id: Iafa23d375713f896add8cebd5a37aea9d41df5fe
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7025,6 +7025,11 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String DISABLE_STACKED_MOBILE_ICONS = "disable_stacked_mobile_icons";
+
+        /**
+         * @hide
+         */
         public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
