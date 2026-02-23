0e6a5ac73e65b7ccc8f894878ecb91ac05de9a51
ChrisCatto <chriscatto@proton.me>
2026-02-23 08:48:51 +0000

    SystemUI: Drop Network traffic mode
    This reverts commit 0ccb98d58fa14eade6173661f6f193a8eee33505.
    
    Change-Id: I2fa6cca712f78974e1a183b40c586833b45ae285
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -112,8 +112,6 @@ public class PhoneStatusBarPolicy
 
     private static final String BLUETOOTH_SHOW_BATTERY =
             "system:" + Settings.System.BLUETOOTH_SHOW_BATTERY;
-    private static final String NETWORK_TRAFFIC_ENABLED =
-            "system:" + Settings.System.NETWORK_TRAFFIC_ENABLED;
 
     private final String mSlotHotspot;
     private final String mSlotBluetooth;
@@ -131,7 +129,6 @@ public class PhoneStatusBarPolicy
     private final String mSlotConnectedDisplay;
     private final String mSlotFirewall;
     private final String mSlotNfc;
-    private final String mSlotNetworkTraffic;
     private final int mDisplayId;
     private final SharedPreferences mSharedPreferences;
     private final DateFormatUtil mDateFormatUtil;
@@ -182,8 +179,6 @@ public class PhoneStatusBarPolicy
     private TunerService mTunerService;
 
     private boolean mShowBluetoothBattery;
-
-    private boolean mShowNetworkTraffic;
 
     @Inject
     public PhoneStatusBarPolicy(Context context, StatusBarIconController iconController,
@@ -255,7 +250,6 @@ public class PhoneStatusBarPolicy
                 com.android.internal.R.string.status_bar_screen_record);
         mSlotFirewall = resources.getString(R.string.status_bar_firewall_slot);
         mSlotNfc = resources.getString(com.android.internal.R.string.status_bar_nfc);
-        mSlotNetworkTraffic = resources.getString(com.android.internal.R.string.status_bar_network_traffic);
 
         mDisplayId = displayId;
         mSharedPreferences = sharedPreferences;
@@ -352,11 +346,6 @@ public class PhoneStatusBarPolicy
         mIconController.setIconVisibility(mSlotNfc, false);
         updateNfc();
 
-        // network traffic
-        mShowNetworkTraffic = Settings.System.getIntForUser(mContext.getContentResolver(),
-            NETWORK_TRAFFIC_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        mIconController.setNetworkTraffic(mSlotNetworkTraffic);
-
         mRotationLockController.addCallback(this);
         mBluetooth.addCallback(this);
         mProvisionedController.addCallback(this);
@@ -377,7 +366,6 @@ public class PhoneStatusBarPolicy
         mCommandQueue.addCallback(this);
 
         mTunerService.addTunable(this, BLUETOOTH_SHOW_BATTERY);
-        mTunerService.addTunable(this, NETWORK_TRAFFIC_ENABLED);
     }
 
     private String getManagedProfileAccessibilityString() {
@@ -428,11 +416,6 @@ public class PhoneStatusBarPolicy
                 mShowBluetoothBattery =
                         TunerService.parseIntegerSwitch(newValue, true);
                 updateBluetooth();
-                break;
-            case NETWORK_TRAFFIC_ENABLED:
-                mShowNetworkTraffic =
-                        TunerService.parseIntegerSwitch(newValue, false);
-                updateNetworkTraffic();
                 break;
             default:
                 break;
@@ -556,14 +539,6 @@ public class PhoneStatusBarPolicy
 
         mIconController.setIcon(mSlotBluetooth, iconId, contentDescription);
         mIconController.setIconVisibility(mSlotBluetooth, bluetoothVisible);
-    }
-
-    private final void updateNetworkTraffic() {
-        if (mShowNetworkTraffic) {
-            mIconController.setNetworkTraffic(mSlotNetworkTraffic);
-        } else {
-            mIconController.removeIcon(mSlotNetworkTraffic, 0);
-        }
     }
 
     private final void updateTTY() {
