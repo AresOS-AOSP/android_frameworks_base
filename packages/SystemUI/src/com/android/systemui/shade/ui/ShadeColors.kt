df907a2564ebe685b385bedccf1a1d4929c7a3c2
ChrisCatto <chriscatto@proton.me>
2026-02-23 11:16:21 +0000

    Revert "SystemUI: Improve shade fallback colors"
    This reverts commit 11b2f8b35faafb02f00cdee82d64a246eb76c7a2.
    
    Change-Id: I37468f6f1e2642ebf9fe02e4fb33178be45837ae

--- packages/SystemUI/src/com/android/systemui/shade/ui/ShadeColors.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ui/ShadeColors.kt
@@ -73,8 +73,7 @@ object ShadeColors {
 
     @JvmStatic
     private fun shadePanelFallback(context: Context): Int {
-        return ColorUtils.blendARGB(context.getColor(R.color.shade_panel_fallback_fg),
-            context.getColor(R.color.shade_panel_fallback_bg), 0.7f)
+        return context.getColor(R.color.shade_panel_fallback)
     }
 
     @JvmStatic
@@ -87,7 +86,6 @@ object ShadeColors {
 
     @JvmStatic
     private fun notificationScrimFallback(context: Context): Int {
-        return ColorUtils.blendARGB(context.getColor(R.color.notification_scrim_base),
-            context.getColor(R.color.shade_panel_fallback_bg), 0.7f)
+        return context.getColor(R.color.notification_scrim_fallback)
     }
 }
