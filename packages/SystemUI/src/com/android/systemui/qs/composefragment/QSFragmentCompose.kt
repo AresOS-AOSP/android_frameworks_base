6ca21571f55e1d2c209d68e989d6f9cc27f0a4d6
Ghosuto <clash.raja10@gmail.com>
2026-03-30 08:15:10 +0000

    SystemUI: Allow disable qs media player [1/2]
    Change-Id: I022c3d4c25f7fb26d9f0c676cd8f7ed673b5c906
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
+++ packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
@@ -27,6 +27,7 @@ import android.graphics.Rect
 import android.os.Bundle
 import android.os.Trace
 import android.os.UserHandle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -1517,6 +1518,21 @@ private fun ContentScope.MediaObject(
 }
 
 @Composable
+fun rememberShowMediaPlayer(): Boolean {
+    val context = LocalContext.current
+    return remember {
+        val cr = context.contentResolver
+        try {
+            Settings.Secure.getIntForUser(
+                cr, Settings.Secure.QS_SHOW_MEDIA_PLAYER, 1, UserHandle.USER_CURRENT
+            ) == 1
+        } catch (_: Throwable) {
+            true
+        }
+    }
+}
+
+@Composable
 @VisibleForTesting
 fun QuickQuickSettingsLayout(
     brightness: @Composable () -> Unit,
@@ -1527,13 +1543,14 @@ fun QuickQuickSettingsLayout(
     val brightnessSettings = rememberQsBrightnessSettings()
     val sliderAtTop = brightnessSettings.sliderAtTop
     val showSlider = brightnessSettings.showSlider
+    val showMediaPlayer = rememberShowMediaPlayer()
 
     Column(verticalArrangement = spacedBy(dimensionResource(R.dimen.qs_tile_margin_vertical))) {
         if (showSlider == 2 && sliderAtTop) {
             brightness()
         }
 
-        if (mediaInRow) {
+        if (mediaInRow && showMediaPlayer) {
             Row(
                 horizontalArrangement = spacedBy(dimensionResource(R.dimen.qs_tile_margin_vertical)),
                 verticalAlignment = Alignment.CenterVertically,
@@ -1549,7 +1566,7 @@ fun QuickQuickSettingsLayout(
             brightness()
         }
 
-        if (!mediaInRow) {
+        if (showMediaPlayer && !mediaInRow) {
             media()
         }
     }
@@ -1567,6 +1584,7 @@ fun QuickSettingsLayout(
     val brightnessSettings = rememberQsBrightnessSettings()
     val sliderAtTop = brightnessSettings.sliderAtTop
     val showSlider = brightnessSettings.showSlider
+    val showMediaPlayer = rememberShowMediaPlayer()
 
     Column(
         verticalArrangement = spacedBy(dimensionResource(R.dimen.qs_tile_margin_vertical)),
@@ -1576,7 +1594,7 @@ fun QuickSettingsLayout(
             brightness()
         }
 
-        if (mediaInRow) {
+        if (mediaInRow && showMediaPlayer) {
             Row(
                 horizontalArrangement = spacedBy(QuickSettingsShade.Dimensions.Padding),
                 verticalAlignment = Alignment.CenterVertically,
@@ -1592,7 +1610,7 @@ fun QuickSettingsLayout(
             brightness()
         }
 
-        if (!mediaInRow) {
+        if (showMediaPlayer && !mediaInRow) {
             media()
         }
     }
