174a4f536e57b6d8a64a9d3b9ea0f2a1ecb0ae9e
ChrisCatto <chriscatto@proton.me>
2026-05-10 11:27:01 +0300

    base: Drop crDroid pif changes
    Revert "core: pif: Remove tensor device check"
    
    This reverts commit 6ff65fec9d6c2f5d4840b3c3607c8f0907306204.
    
    Revert "core: pif: Enable photos spoof by default"
    
    This reverts commit dff7fc0160f8207edf7d2cce118c94d1d34a4a7b.
    
    Revert "services: spoof: Read settings from current user"
    
    This reverts commit 0474aff2a42453bfaef77456c9cfe009a5b33eb5.
    
    Revert "core: pif: Add separate tunable for gphotos spoof"
    
    This reverts commit cc3b982cea0e76886726b933e4c4ade1ac72f978.
    
    Change-Id: I452c3203aac37b0a63a00e2c83c6b3bbec977f26
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19784,11 +19784,6 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
 
     @Override
-    public String getSpoofPifSpoofPhotos() {
-        return AxExtServiceFactory.getSpoofManager().getPifSpoofPhotos();
-    }
-
-    @Override
     public String getSpoofGamePropsConfig() {
         return AxExtServiceFactory.getSpoofManager().getGamePropsConfig();
     }
